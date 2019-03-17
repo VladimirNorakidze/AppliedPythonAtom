364d0e98f6916bb3221d091bff7490e026883cb6
Владимир Норакидзе <vladimir@MacBook-Pro-Vladimir.local>
2019-03-17 19:40:13 +0300

    HW3: event_stats, hashmap, hashset, lrucache

--- homeworks/homework_03/hw3_hashset.py
+++ homeworks/homework_03/hw3_hashset.py
@@ -4,29 +4,33 @@
 from homeworks.homework_03.hw3_hashmap import HashMap
 
 
-class HashSet:
+class HashSet(HashMap):
 
-    def __init__(self):
+    def __init__(self, bucket_num=64):
         # TODO Сделать правильно =)
-        raise NotImplementedError
+        super().__init__(bucket_num=bucket_num)
 
-    def get(self, key, default_value=None):
+    def get(self, value, default_value=None):
         # TODO достаточно переопределить данный метод
-        raise NotImplementedError
+        return super().get(value, default_value=default_value)
 
-    def put(self, key, value):
+    def put(self, value):
         # TODO метод put, нужно переопределить данный метод
-        raise NotImplementedError
+        super().put(value, value)
 
     def __len__(self):
         # TODO Возвращает количество Entry в массиве
-        raise NotImplementedError
+        return self.capacity
 
     def values(self):
         # TODO возвращать итератор значений
-        raise NotImplementedError
+        return super().values()
 
     def intersect(self, another_hashset):
         # TODO метод, возвращающий новый HashSet
         #  элементы - пересечение текущего и другого
-        raise NotImplementedError
+        new_hashset = HashSet()
+        for element in another_hashset.keys():
+            if element in self.keys():
+                new_hashset.put(element)
+        return new_hashset
--- homeworks/homework_03/hw3_lrucache.py
+++ homeworks/homework_03/hw3_lrucache.py
@@ -3,6 +3,8 @@
 
 
 import time
+import collections
+
 
 class LRUCacheDecorator:
 
@@ -16,8 +18,40 @@ class LRUCacheDecorator:
         #  https://www.geeksforgeeks.org/class-as-decorator-in-python/
         self.maxsize = maxsize
         self.ttl = ttl
-        raise NotImplementedError
+        self._args_cache = collections.deque([])
+        self._time_cache = collections.deque([])
+        self._result_cache = collections.deque([])
+
+    def _clearing_cache(self):
+        while True:
+            if time.time() - self._time_cache[-1] > self.ttl:
+                self._time_cache.pop()
+                self._args_cache.pop()
+                self._result_cache.pop()
+
+    def _checking_cache(self, args, kwargs):
+        for i, var in enumerate(self._args_cache):
+            if args == var[0]:
+                return [True, i]
+        else:
+            self._put_cache(args, kwargs)
+            return [False, -1]
+
+    def _put_cache(self, args, kwargs):
+        self._args_cache.appendleft((args, kwargs))
+        self._time_cache.appendleft(time.time())
 
     def __call__(self, *args, **kwargs):
         # TODO вызов функции
-        raise NotImplementedError
+        if callable(args[0]):
+            self.function = args[0]
+        else:
+            if self._time_cache:
+                self._clearing_cache()
+            check_status, idx = self._checking_cache(args, kwargs)
+            if check_status:
+                return self._result_cache[idx]
+            else:
+                result = self.function(*args, **kwargs)
+                self._result_cache.appendleft(result)
+                return result
