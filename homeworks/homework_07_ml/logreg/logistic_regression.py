41f6f6585cbbaa14cf657c65c947966c17b00218
RocknRolla <myemail@sobaka.com>
2019-04-23 17:26:37 +0300

    ...

--- homeworks/homework_07_ml/logreg/logistic_regression.py
+++ homeworks/homework_07_ml/logreg/logistic_regression.py
@@ -7,20 +7,20 @@ from sklearn import metrics
 
 
 class LogisticRegression:
-    def __init__(self, n_iter=100, lambda_coef=1.0, regulatization=None, alpha=0.5):
+    def __init__(self, lambda_coef=1.0, regularization=None, alpha=0.5):
         """
         LogReg for Binary case
         :param lambda_coef: constant coef for gradient descent step
         :param regulatization: regularizarion type ("L1" or "L2") or None
         :param alpha: regularizarion coefficent
         """
-        self.n_iter = n_iter
         self.lambda_coef = lambda_coef
-        self.regulatization = regulatization
+        self.regularization = regularization
         self.alpha = alpha
         self.weights = np.array([])
+        self.learned = False
 
-    def fit(self, x_train, y_train):
+    def fit(self, x_train, y_train, n_iter=50):
         """
         Fit model using gradient descent method
         :param x_train: training data
@@ -29,14 +29,22 @@ class LogisticRegression:
         """
         x_train = np.concatenate((np.ones((x_train.shape[0], 1)), x_train), axis=1)
         self.weights = np.ones((x_train.shape[1], np.amax(y_train) + 1))
-        for n in np.arange(self.n_iter):
+        der_amend = 0
+        if self.regularization == 'L1':
+            der_amend = 2 * self.alpha * self.weights
+        elif self.regularization == 'L2':
+            der_amend = self.alpha * np.ones((self.weights.shape[0], self.weights.shape[1]))
+        elif self.regularization is None:
+            der_amend = np.zeros((self.weights.shape[0], self.weights.shape[1]))
+        for n in np.arange(n_iter):
             for i in np.arange(self.weights.shape[0]):
                 for j in np.arange(self.weights.shape[1]):
                     for m in np.arange(y_train.shape[0]):
                         self.weights[i, j] -= self.lambda_coef * np.exp(x_train[m, :] @ self.weights[:, j]) *\
-                                              x_train[m, i] / np.sum(np.exp(x_train[m, :] @ self.weights))
+                                              x_train[m, i] / np.sum(np.exp(x_train[m, :] @ self.weights)) + der_amend[i, j]
                         if j == y_train[m, 0]:
                             self.weights[i, j] += self.lambda_coef * x_train[m, i]
+        self.learned = True
 
     def predict(self, x_test):
         """
@@ -44,12 +52,15 @@ class LogisticRegression:
         :param x_test: test data for predict in
         :return: y_test: predicted values
         """
-        x_test = np.concatenate((np.ones((x_test.shape[0], 1)), x_test), axis=1)
-        y_pred = x_test @ self.weights
-        for i in np.arange(y_pred.shape[0]):
-            y_pred[i, :] = np.exp(y_pred[i, :]) / np.sum(np.exp(y_pred[i, :]))
-        y_pred = np.argmax(y_pred, axis=1).reshape(-1, 1)
-        return y_pred
+        if self.learned:
+            x_test = np.concatenate((np.ones((x_test.shape[0], 1)), x_test), axis=1)
+            y_pred = x_test @ self.weights
+            for i in np.arange(y_pred.shape[0]):
+                y_pred[i, :] = np.exp(y_pred[i, :]) / np.sum(np.exp(y_pred[i, :]))
+            y_pred = np.argmax(y_pred, axis=1).reshape(-1, 1)
+            return y_pred
+        else:
+            print("Model isn't learned")
 
     def predict_proba(self, x_test):
         """
@@ -57,31 +68,21 @@ class LogisticRegression:
         :param x_test: test data for predict in
         :return: y_test: predicted probabilities
         """
-        x_test = np.concatenate((np.ones((x_test.shape[0], 1)), x_test), axis=1)
-        y_pred = x_test @ self.weights
-        for i in np.arange(y_pred.shape[0]):
-            y_pred[i, :] = np.exp(y_pred[i, :])/np.sum(np.exp(y_pred[i, :]))
-        return y_pred
+        if self.learned:
+            x_test = np.concatenate((np.ones((x_test.shape[0], 1)), x_test), axis=1)
+            y_pred = x_test @ self.weights
+            for i in np.arange(y_pred.shape[0]):
+                y_pred[i, :] = np.exp(y_pred[i, :])/np.sum(np.exp(y_pred[i, :]))
+            return y_pred
+        else:
+            print("Model isn't learned")
 
     def get_weights(self):
         """
         Get weights from fitted linear model
         :return: weights array
         """
-        return self.weights
-
-
-# x_train = np.random.randint(low=-9, high=10, size=(6, 3))
-# x_train_ones = np.concatenate((np.ones((x_train.shape[0], 1)), x_train), axis=1)
-# weights = np.random.randint(low=-9, high=10, size=(4, 3))
-# y_train = np.argmax(x_train_ones @ weights, axis=1).reshape(-1, 1)
-# x_test = np.random.randint(low=-9, high=10, size=(6, 3))
-# x_test_ones = np.concatenate((np.ones((x_test.shape[0], 1)), x_test), axis=1)
-# y_true = np.argmax(x_test_ones @ weights, axis=1).reshape(-1, 1)
-# test = LogisticRegression(lambda_coef=2)
-# test.fit(x_train, y_train)
-# print(test.predict_proba(x_test))
-# for i in np.arange(y_true.shape[0]):
-#     print(test.predict(x_test)[i, :], end=' ')
-#     print(y_true[i, :])
-# print(metrics.log_loss(y_true, test.predict_proba(x_test)))
+        if self.learned:
+            return self.weights
+        else:
+            print("Model isn't learned")
