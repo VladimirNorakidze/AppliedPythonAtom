157e7de3adcfed600bb304c9c853f809d136be66
Владимир Норакидзе <vladimir@MacBook-Pro-Vladimir.local>
2019-05-13 21:38:36 +0300

    HW6_ML: metrics

--- homeworks/homework_06_ml/metrics.py
+++ homeworks/homework_06_ml/metrics.py
@@ -12,7 +12,8 @@ def mse(y_true, y_hat, derivative=False):
     :param y_hat: vector of estimated target values
     :return: loss
     """
-    pass
+    mse_loss = np.sum((y_true[:] - y_hat[:])**2) / len(y_true)
+    return mse_loss
 
 
 def mae(y_true, y_hat):
@@ -22,7 +23,8 @@ def mae(y_true, y_hat):
     :param y_hat: vector of estimated target values
     :return: loss
     """
-    pass
+    mae_loss = np.sum(np.abs(y_true[:] - y_hat[:])) / len(y_true)
+    return mae_loss
 
 
 def r2_score(y_true, y_hat):
@@ -32,4 +34,8 @@ def r2_score(y_true, y_hat):
     :param y_hat: vector of estimated target values
     :return: loss
     """
-    pass
+    mean_y = np.sum(y_true) / len(y_true)
+    ss_tot = np.sum((y_true[:] - mean_y)**2)
+    ss_res = mse(y_true, y_hat) * len(y_true)
+    r2_score_loss = 1 - ss_res / ss_tot
+    return r2_score_loss
